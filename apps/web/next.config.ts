4c49a5a7ae0680a229617fcfd5e041e91fbcb12a
Jeremias Santos <jeremiassantos@Mac-mini-de-Jeremias.local>
2026-02-05 14:33:13 -0300

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@tatame-monorepo/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
