d230c2a9bd3c4dea081375ea3c7793b429f6f6b1
Jeremias Santos <jeremiassnts3@gmail.com>
2026-02-07 19:42:29 -0300

    feat: add webhooks to invoice and trial end

--- apps/server/src/routes/webhooks.ts
+++ apps/server/src/routes/webhooks.ts
@@ -10,6 +10,15 @@ webhooksRouter.post("/stripe", async (req, res, next) => {
             case "customer.subscription.created":
                 await stripeWebhooksService.handleSubscriptionCreated(data);
                 break;
+            case "customer.subscription.trial_will_end":
+                await stripeWebhooksService.handleSubscriptionTrialWillEnd(data);
+                break;
+            case "invoice.paid":
+                await stripeWebhooksService.handleInvoicePaid(data);
+                break;
+            case "invoice.payment_failed":
+                await stripeWebhooksService.handleInvoicePaymentFailed(data);
+                break;
             default:
                 break;
         }
--- apps/server/src/services/stripe/webhooks.ts
+++ apps/server/src/services/stripe/webhooks.ts
@@ -18,5 +18,59 @@ export const stripeWebhooksService = {
             "PLAN": product.name ?? "",
             "TRIALEND": format(new Date(trialEnd * 1000), "dd/MM/yyyy")
         })
+    },
+    handleSubscriptionTrialWillEnd: async (data: any) => {
+        const customerId = data.object.customer;
+        const trialEnd = data.object.trial_end;
+        const [item] = data.object.items.data;
+        const productId = item.price.product
+
+        const customer = await stripeService.getCustomer(customerId);
+        const product = await stripeService.getProduct(productId);
+
+        const resendService = new ResendService();
+        await resendService.sendEmailWithTemplate("trial-end-warning", customer.email!, {
+            "CLIENT": customer.name ?? "",
+            "PLAN": product.name ?? "",
+            "TRIALEND": format(new Date(trialEnd * 1000), "dd/MM/yyyy")
+        })
+    },
+    handleInvoicePaid: async (data: any) => {
+        const customerId = data.object.customer;
+        const amount = data.object.amount_paid;
+        if (amount <= 0) {
+            return;
+        }
+        const subscriptionId = data.object.parent.subscription_details.subscription;
+        const currency = data.object.currency;
+
+        const customer = await stripeService.getCustomer(customerId);
+        const subscription = await stripeService.getSubscription(subscriptionId);
+
+        const resendService = new ResendService();
+        await resendService.sendEmailWithTemplate("invoice-paid", customer.email!, {
+            "CLIENT": customer.name ?? "",
+            "PLAN": subscription.items.data[0]?.price.product.toString() ?? "",
+            "PRICE": `${currency.toUpperCase()} $${(amount / 100).toFixed(2).toString()}`
+        })
+    },
+    handleInvoicePaymentFailed: async (data: any) => {
+        const customerId = data.object.customer;
+        const amount = data.object.amount_paid;
+        if (amount <= 0) {
+            return;
+        }
+        const subscriptionId = data.object.parent.subscription_details.subscription;
+        const currency = data.object.currency;
+
+        const customer = await stripeService.getCustomer(customerId);
+        const subscription = await stripeService.getSubscription(subscriptionId);
+
+        const resendService = new ResendService();
+        await resendService.sendEmailWithTemplate("invoice-payment-failed", customer.email!, {
+            "CLIENT": customer.name ?? "",
+            "PLAN": subscription.items.data[0]?.price.product.toString() ?? "",
+            "PRICE": `${currency.toUpperCase()} $${(amount / 100).toFixed(2).toString()}`
+        })
     }
 }
